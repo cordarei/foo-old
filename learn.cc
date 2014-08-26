fc780e58ecfeffb1715ea6ab851e3c8a4a285002
Joseph Irwin <joseph.irwin.gt@gmail.com>
2014-08-26 16:58:30 +0900

    Pretty-print trees as they are read
    For debugging purposes.

--- learn.cc
+++ learn.cc
@@ -33,7 +33,7 @@ tree read_texpr(std::istream &in) {
     auto cur = buff.cbegin();
     auto end = buff.cend();
 
-    std::cerr << "# " << buff << std::endl;
+    // std::cerr << "# " << buff << std::endl;
 
     while (cur != end) {
       auto next_paren = std::find_if(cur, end, [](char c) { return c == '(' or c == ')'; });
@@ -76,13 +76,55 @@ tree read_texpr(std::istream &in) {
 }
 
 
-int main(int argc, char** argv)
-{
+/*
+ * pretty-print a tree to the given ostream
+ *
+ * believe me, there are infinitely better ways to do this
+ */
+std::ostream& operator<<(std::ostream &out, const tree &t) {
+  std::vector<std::pair<int,tree const *>> stack{std::make_pair(0, &t)};
+
+  while(!stack.empty()) {
+    auto p = stack.back();
+    stack.pop_back();
+    for (int i = p.first; i > 0; --i)
+      std::cout << ' ';
+    if (!p.second) {
+      std::cout << ')' << std::endl;
+    } else if (p.second->children.size() == 1) {
+      if (p.second->children[0].children.size() == 0) {
+	std::cout << '('
+		  << p.second->label
+		  << ' '
+		  << p.second->children[0].label
+		  << ')'
+		  << std::endl;
+      } else {
+	stack.emplace_back(p.first, &p.second->children[0]);
+      }
+    } else if (p.second->children.size() > 1) {
+      stack.emplace_back(p.first, nullptr);
+      std::cout << '('
+		<< p.second->label
+		<< std::endl;
+      for (auto it = p.second->children.rbegin(); it != p.second->children.rend(); ++it)
+	stack.emplace_back(p.first + 2, &*it);
+    } else {
+      std::cout << "wtf??" << std::endl;
+    }
+  }
+
+  return out;
+}
+
+
+int main(int argc, char** argv) {
   int count = 0;
   try {
     while (true) {
       auto t = read_texpr(std::cin);
       ++count;
+      std::cout << t << std::endl;
     }
   } catch(std::runtime_error e) {
     std::cout << e.what() << std::endl;
