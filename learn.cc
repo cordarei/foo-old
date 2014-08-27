2751d5d790df05fb118295af4c0f6b84e81ea8c8
Joseph Irwin <joseph.irwin.gt@gmail.com>
2014-08-27 16:46:09 +0900

    Untabify (dammit Emacs!)

--- learn.cc
+++ learn.cc
@@ -49,39 +49,39 @@ tree read_texpr(std::istream &in) {
     while (cur != end) {
       auto next_paren = std::find_if(cur, end, [](char c) { return c == '(' or c == ')'; });
       if (cur < next_paren) {
-	if (stack.empty()) {
-	  throw std::runtime_error("text outside tree");
-	}
+        if (stack.empty()) {
+          throw std::runtime_error("text outside tree");
+        }
 
-	if (last_token == tok::lparen) {
-	  stack.back().label = std::string(cur, next_paren);
-	} else if (last_token == tok::label) {
-	  stack.back().children.emplace_back(std::string(cur, next_paren), std::vector<tree>{});
-	} else {
-	  throw std::runtime_error("text after close paren");
-	}
-	last_token = tok::label;
+        if (last_token == tok::lparen) {
+          stack.back().label = std::string(cur, next_paren);
+        } else if (last_token == tok::label) {
+          stack.back().children.emplace_back(std::string(cur, next_paren), std::vector<tree>{});
+        } else {
+          throw std::runtime_error("text after close paren");
+        }
+        last_token = tok::label;
       } else {
-	if ('(' == *next_paren) {
-	  stack.emplace_back(std::string{}, std::vector<tree>{});
-	  last_token = tok::lparen;
-	} else {
-	  last_token = tok::rparen;
-	  tree t = stack.back();
-	  stack.pop_back();
+        if ('(' == *next_paren) {
+          stack.emplace_back(std::string{}, std::vector<tree>{});
+          last_token = tok::lparen;
+        } else {
+          last_token = tok::rparen;
+          tree t = stack.back();
+          stack.pop_back();
 
-	  if (stack.empty()) {
-	    //this assumes that nothing is left in `buff`
-	    if (t.label.empty() && t.children.size() == 1) {
-	      return t.children[0];
-	    } else {
-	      return t;
-	    }
-	  } else {
-	    stack.back().children.push_back(t);
-	  }
-	}
-	++next_paren;
+          if (stack.empty()) {
+            //this assumes that nothing is left in `buff`
+            if (t.label.empty() && t.children.size() == 1) {
+              return t.children[0];
+            } else {
+              return t;
+            }
+          } else {
+            stack.back().children.push_back(t);
+          }
+        }
+        ++next_paren;
       }
 
       cur = next_paren;
@@ -109,30 +109,30 @@ std::ostream& operator<<(std::ostream &out, const tree &t) {
       std::cout << ')' << std::endl;
     } else if (p.second->children.size() == 1) {
       if (p.second->children[0].children.size() == 0) {
-	std::cout << '('
-		  << p.second->label
-		  << ' '
-		  << p.second->children[0].label
-		  << ')'
-		  << std::endl;
+        std::cout << '('
+                  << p.second->label
+                  << ' '
+                  << p.second->children[0].label
+                  << ')'
+                  << std::endl;
       } else {
-	stack.emplace_back(p.first, nullptr);
-	if (!p.second->label.empty()) {
-	  std::cout << '('
-		    << p.second->label
-		    << std::endl;
-	  stack.emplace_back(p.first + 2, &p.second->children[0]);
-	} else {
-	  stack.emplace_back(p.first, &p.second->children[0]);
-	}
+        stack.emplace_back(p.first, nullptr);
+        if (!p.second->label.empty()) {
+          std::cout << '('
+                    << p.second->label
+                    << std::endl;
+          stack.emplace_back(p.first + 2, &p.second->children[0]);
+        } else {
+          stack.emplace_back(p.first, &p.second->children[0]);
+        }
       }
     } else if (p.second->children.size() > 1) {
       stack.emplace_back(p.first, nullptr);
       std::cout << '('
-		<< p.second->label
-		<< std::endl;
+                << p.second->label
+                << std::endl;
       for (auto it = p.second->children.rbegin(); it != p.second->children.rend(); ++it)
-	stack.emplace_back(p.first + 2, &*it);
+        stack.emplace_back(p.first + 2, &*it);
     } else {
       std::cout << "wtf??" << std::endl;
     }
@@ -166,7 +166,7 @@ tree binarize_tree(const tree &t) {
     return tree(t.label, {binarize_tree(t.children[0])});
   case 2:
     return tree(t.label, {binarize_tree(t.children[0]),
-	                  binarize_tree(t.children[1])});
+                          binarize_tree(t.children[1])});
   default:
     break;
   }
